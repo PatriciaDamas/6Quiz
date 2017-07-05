3fe764a73f05ea4190eeabb2321893e0a94d203f
ticha7 <claudiadamas27@gmail.com>
2017-07-05 12:34:08 +0100

    teste

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
 
-    request('https://webitcloud.net/PW/1617/ACJ/wQuizz/view/login.html').pipe(res);
+    // request('https://webitcloud.net/PW/1617/ACJ/wQuizz/view/login.html').pipe(res);
+    res.send("ola")
 
 });
 
