75a80671d7e96aa75adda3cfeae6b82bbdea3ead
ticha7 <claudiadamas27@gmail.com>
2017-07-05 12:30:25 +0100

    v1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-
+var request = require('request');
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
 
-    res.sendfile("https://webitcloud.net/PW/1617/ACJ/wQuizz/view/login.html");
+    request('https://webitcloud.net/PW/1617/ACJ/wQuizz/view/login.html').pipe(res);
 
 });
 
