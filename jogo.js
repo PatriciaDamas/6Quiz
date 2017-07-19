3070b97312cf2924e70cb1a42c8c314de3f49fc5
JoanaFAMendes <janafam@hotmail.com>
2017-07-19 18:11:55 +0100

    v1

--- jogo.js
+++ jogo.js
@@ -90,6 +90,7 @@ $(document).ready(function () {
                     $("#div_tipoJogo").empty();
                     $("#div_tipoJogo2").empty();
                     var perguntasID = 0;
+                    var pontuacaoJoagador = 0;//pontuação do jogador
                     var nomesDiv = [];
                     var idObj = $(this).attr('id');
                     console.log($(this).attr('id'));
@@ -214,7 +215,6 @@ $(document).ready(function () {
                                     respostas += '</div>';
 
                                     $("#div_tipoJogo").append(pergunta + respostas);
-                                    var pontuacaoJoagador = 0;//pontuação do jogador
                                     console.log("a" + pontuacaoJoagador);
                                     for (var k = 0; k < r; k++) {
                                         $("#" + nomesDiv[k]).click(function () {
@@ -236,63 +236,64 @@ $(document).ready(function () {
                                             }
                                         });
                                     }
-                                    $('#div_tipoJogo3').on(function () {
-                                        // do stuff
-                                        var timetogo = 180;
-                                        console.log(timetogo);
 
-                                        var myCounter = new Countdown({
-                                            seconds: timetogo, // number of seconds to count down
-                                            onUpdateStatus: function (sec) {
-                                                elapsed = timetogo - sec;
-                                                $('.progress-bar').width(((elapsed / timetogo) * 100) + "%");
-                                                //tempo = 1;
-                                            }, // callback for each second
-                                            onCounterEnd: function () {
-                                                //alert('counter ended!');
-                                                //tempo = 0;
-
-                                            } // final action
-                                        });
-
-                                        myCounter.start();
-                                    });
-
-                                    var timetogoFirst = 180;
-                                    var myCounterFirst = new Countdown({
-                                        seconds: timetogoFirst, // number of seconds to count down
-                                        onUpdateStatus: function (sec) {
-                                            elapsed = timetogoFirst - sec;
-                                            $('.progress-bar').width(((elapsed / timetogoFirst) * 100) + "%");
-
-                                            //tempo = 1;
-                                        }, // callback for each second
-                                        onCounterEnd: function () {
-                                            //alert('counter ended!');
-                                            //tempo = 0;
-
-                                           /* $.ajax({
-                                                ype: "GET",
-                                                url: urlc + "pontuacao?pontos=" + pontuacaoJoagador,
-                                                contentType: "application/json"
-                                            });*/
-
-                                        } // final action
-                                    });
-                                    myCounterFirst.start();
-
-                                    if (timetogoFirst == 0) {
-                                        $.ajax({
-                                            ype: "GET",
-                                            url: urlc + "pontuacao?pontos=" + pontuacaoJoagador,
-                                            contentType: "application/json"
-                                        });
-                                        //location.href = 'https://wquizz.herokuapp.com/app';
-                                    }
                                 });
 
                             });//fim perguntas
                         });
+                        $('#div_tipoJogo3').on(function () {
+                            // do stuff
+                            var timetogo = 180;
+                            console.log(timetogo);
+
+                            var myCounter = new Countdown({
+                                seconds: timetogo, // number of seconds to count down
+                                onUpdateStatus: function (sec) {
+                                    elapsed = timetogo - sec;
+                                    $('.progress-bar').width(((elapsed / timetogo) * 100) + "%");
+                                    //tempo = 1;
+                                }, // callback for each second
+                                onCounterEnd: function () {
+                                    //alert('counter ended!');
+                                    //tempo = 0;
+
+                                } // final action
+                            });
+
+                            myCounter.start();
+                        });
+
+                        var timetogoFirst = 180;
+                        var myCounterFirst = new Countdown({
+                            seconds: timetogoFirst, // number of seconds to count down
+                            onUpdateStatus: function (sec) {
+                                elapsed = timetogoFirst - sec;
+                                $('.progress-bar').width(((elapsed / timetogoFirst) * 100) + "%");
+
+                                //tempo = 1;
+                            }, // callback for each second
+                            onCounterEnd: function () {
+                                //alert('counter ended!');
+                                //tempo = 0;
+
+                                /* $.ajax({
+                                     ype: "GET",
+                                     url: urlc + "pontuacao?pontos=" + pontuacaoJoagador,
+                                     contentType: "application/json"
+                                 });*/
+
+                            } // final action
+                        });
+                        myCounterFirst.start();
+
+                        if (timetogoFirst == 0) {
+                            $.ajax({
+                                ype: "GET",
+                                url: urlc + "pontuacao?pontos=" + pontuacaoJoagador,
+                                contentType: "application/json"
+                            });
+                            //location.href = 'https://wquizz.herokuapp.com/app';
+                        }
 
 
                     }//fim for dificuldade
