a10e6ea304befd016aa0b512e5f045b9ca7f5c89
JoanaFAMendes <janafam@hotmail.com>
2017-07-22 21:38:21 +0100

    v1

--- jogo.js
+++ jogo.js
@@ -179,7 +179,7 @@ $(document).ready(function () {
                                             // respostas += '<div class="col-sm-6"><center><div class="div_resposta" id="' + data.id_resposta + '"><button type="button" class="btn" id="btn_resposta">' + data.resposta + '</button></div></center></div>';
 
 
-                                            respostas += '<input type="radio" class="hidden-inputs" name="option' + p + '" value="' + data.resposta + '"><span class="input-group-addon" id="labelRes'+p+'">' + data.resposta + '</span>'
+                                            respostas += '<input type="radio" class="hidden-inputs" name="option' + p + '" id="labelRes'+p+'" value="' + data.resposta + '"><span class="input-group-addon" id="labelRes'+p+'">' + data.resposta + '</span>'
                                             // $("#div_resp").css("text-align", "left");
                                             // <span class="input-group-title" id="resp" >'+  data.resposta+'</span>
 
