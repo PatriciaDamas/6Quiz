7909029381f5bd26d12438faccd0015ae28e0763
JoanaFAMendes <janafam@hotmail.com>
2017-07-22 20:36:53 +0100

    v1

--- jogo.js
+++ jogo.js
@@ -218,7 +218,73 @@ $(document).ready(function () {
                                     respostas += '</div>';
 
                                     $("#div_tipoJogo").append(pergunta + respostas);
+                                    $('#div_tipoJogo3').on(function () {
+                                        // do stuff
+                                        var timetogo = 180;
+                                        console.log(timetogo);
+
+                                        var myCounter = new Countdown({
+                                            seconds: timetogo, // number of seconds to count down
+                                            onUpdateStatus: function (sec) {
+                                                elapsed = timetogo - sec;
+                                                $('.progress-bar').width(((elapsed / timetogo) * 100) + "%");
+                                                //tempo = 1;
+                                            }, // callback for each second
+                                            onCounterEnd: function () {
+                                                //alert('counter ended!');
+                                                //tempo = 0;
+
+                                            } // final action
+                                        });
+
+                                        myCounter.start();
+                                    });
+
+                                    var timetogoFirst = 180;
+                                    var myCounterFirst = new Countdown({
+                                        seconds: timetogoFirst, // number of seconds to count down
+                                        onUpdateStatus: function (sec) {
+                                            elapsed = timetogoFirst - sec;
+                                            $('.progress-bar').width(((elapsed / timetogoFirst) * 100) + "%");
+                                            console.log("time1: " + sec);
+                                            console.log("timeE: " + elapsed);
+                                            //tempo = 1;
+                                        }, // callback for each second
+                                        onCounterEnd: function () {
+                                            //alert('counter ended!');
+                                            //tempo = 0;
+                                            var pontuacaoJoagador = 0;//pontuação do jogador
+                                            console.log("a" + pontuacaoJoagador);
+                                            for (var k = 0; k < p; k++) {
+                                                if ($("input:radio[name='" + nemesDiv[k] + "']").is(":checked")) {
+                                                    var idResp = $(this).attr('value');
+                                                    for (var d = 0; d < v; d++) {
+                                                        if (idResp == validade[d]) {
+                                                            console.log("true");
+                                                            console.log("fsa" + idResp + "fdsf" + validade[d]);
+                                                            //$("#" + idResp).css('background-color', '#00cc00');
+                                                            console.log("hda" + pontuacaoP[v]);
+                                                            pontuacaoJoagador += 30;//não estava a ir buscar a poontuação da resposta
+                                                            //pontuacaoJoagador += pontuacaoP[v];
+                                                            console.log("k" + pontuacaoJoagador);
+                                                        }
+                                                    }
+                                                }
+                                            }
+
+                                            console.log("pontos");
+                                            $.ajax({
+                                                ype: "GET",
+                                                url: urlc + "pontuacao?pontos=" + pontuacaoJoagador,
+                                                contentType: "application/json"
+                                            });
+                                            //location.href = 'https://wquizz.herokuapp.com/app';
+
+                                        } // final action
+                                    });
+                                    myCounterFirst.start();
                                 });
+
 
                             });//fim perguntas
                         });
@@ -227,71 +293,7 @@ $(document).ready(function () {
                     }//fim for dificuldade
 
 
-                    $('#div_tipoJogo3').on(function () {
-                        // do stuff
-                        var timetogo = 180;
-                        console.log(timetogo);
 
-                        var myCounter = new Countdown({
-                            seconds: timetogo, // number of seconds to count down
-                            onUpdateStatus: function (sec) {
-                                elapsed = timetogo - sec;
-                                $('.progress-bar').width(((elapsed / timetogo) * 100) + "%");
-                                //tempo = 1;
-                            }, // callback for each second
-                            onCounterEnd: function () {
-                                //alert('counter ended!');
-                                //tempo = 0;
-
-                            } // final action
-                        });
-
-                        myCounter.start();
-                    });
-
-                    var timetogoFirst = 180;
-                    var myCounterFirst = new Countdown({
-                        seconds: timetogoFirst, // number of seconds to count down
-                        onUpdateStatus: function (sec) {
-                            elapsed = timetogoFirst - sec;
-                            $('.progress-bar').width(((elapsed / timetogoFirst) * 100) + "%");
-                            console.log("time1: " + sec);
-                            console.log("timeE: " + elapsed);
-                            //tempo = 1;
-                        }, // callback for each second
-                        onCounterEnd: function () {
-                            //alert('counter ended!');
-                            //tempo = 0;
-                            var pontuacaoJoagador = 0;//pontuação do jogador
-                            console.log("a" + pontuacaoJoagador);
-                            for (var k = 0; k < p; k++) {
-                                if ($("input:radio[name='" + nemesDiv[k] + "']").is(":checked")) {
-                                    var idResp = $(this).attr('value');
-                                    for (var d = 0; d < v; d++) {
-                                        if (idResp == validade[d]) {
-                                            console.log("true");
-                                            console.log("fsa" + idResp + "fdsf" + validade[d]);
-                                            //$("#" + idResp).css('background-color', '#00cc00');
-                                            console.log("hda" + pontuacaoP[v]);
-                                            pontuacaoJoagador += 30;//não estava a ir buscar a poontuação da resposta
-                                            //pontuacaoJoagador += pontuacaoP[v];
-                                            console.log("k" + pontuacaoJoagador);
-                                        }
-                                    }
-                                }
-                            }
-
-                            console.log("pontos");
-                            $.ajax({
-                                ype: "GET",
-                                url: urlc + "pontuacao?pontos=" + pontuacaoJoagador,
-                                contentType: "application/json"
-                            });
-                            //location.href = 'https://wquizz.herokuapp.com/app';
-
-                        } // final action
-                    });
-                    myCounterFirst.start();
 
 
 
